1a2fabf317ba6a7ec1f9922d16240e6809631949
ExpressDHL <Liverpool-win@windowslive.com>
2025-07-03 17:13:53 +0000

    1

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,9 +1,9 @@
 function build_url(){
     var url = encodeURI(decodeURI(document.querySelector('#url').value));
     if (url === "" || url.indexOf("http") === -1){
-        document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
+        document.getElementById("b_url").innerHTML=`NO1`;;
     }
-    else {
+    else { 
         url = document.location.href + "api/?url=" + url;
         document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
     }
