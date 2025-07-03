16f5fbfb373fbc277674925fc917e2c2f2f0dcff
ExpressDHL <Liverpool-win@windowslive.com>
2025-07-03 17:10:11 +0000

    remove sth

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,6 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/01.webp': 'center',
-							'images/02.webp': 'center',
-							'images/03.webp': 'center'
 						},
 
 					// Delay.
